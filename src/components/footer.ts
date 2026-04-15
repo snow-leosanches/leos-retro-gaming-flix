31c0ab8089e0b167fb781c1a0b04f6840e99c52a
Leo Sanches <leonel.sanches@snowplowanalytics.com>
2026-04-14 17:44:06 -0700

    `user_id` should never be equal to email address.

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,4 +1,4 @@
-import { trackCustomerIdentification, setSnowplowUserId, isSnowplowEnabled } from '../analytics/snowplow'
+import { trackCustomerIdentification, isSnowplowEnabled } from '../analytics/snowplow'
 
 function isValidEmail(email: string): boolean {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
@@ -45,7 +45,6 @@ export function createFooter(): HTMLElement {
       input.focus()
       return
     }
-    setSnowplowUserId(email)
     trackCustomerIdentification(email)
     showFeedback(isSnowplowEnabled() ? 'Subscribed!' : 'Subscribed! (tracking disabled)')
     input.value = ''
